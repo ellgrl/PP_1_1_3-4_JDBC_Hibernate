8ea52bd401ff2ab359fa204eda5013ccfd8fef72
ellgrl <lzoik@mail.ru>
2025-02-27 15:27:01 +0300

    task completed

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,9 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,5 +26,7 @@ public class Main {
         userService.cleanUsersTable();
 
         userService.dropUsersTable();
+
+        Util.closeConnection();
     }
 }
